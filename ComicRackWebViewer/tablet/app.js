8891eeede17710034c2aa33e830f7c4d87409c0b
Jeroen Walter <jeroen@enormkansloos.nl>
2012-09-08 20:38:12 +0200

    version 1.9

--- ComicRackWebViewer/tablet/app.js
+++ ComicRackWebViewer/tablet/app.js
@@ -65,6 +65,11 @@ Ext.application({
         '1496x2048': 'resources/startup/1496x2048.jpg'
     },
     
+    /*
+    viewport: {
+      autoMaximize: true
+    },
+    */
     //profiles: ['Phone', 'Tablet'],
     
     views: ['Main'],
@@ -109,4 +114,17 @@ Ext.application({
         );
     }
 });
+/*
+function hideAddressBar() {
+    setTimeout(function() {
+        window.scrollTo(0, 1);
+    }, 1000); // The delay can be tweaked a bit, but is necessary.
+}
 
+window.onresize = function() {
+     if (document.activeElement.tagName.toLowerCase() == 'body') {
+        // Do this check to prevent scrolling when typing in a textfield
+        hideAddressBar();
+    }
+};
+*/
--- ComicRackWebViewer/tablet/app/view/About.js
+++ ComicRackWebViewer/tablet/app/view/About.js
@@ -38,7 +38,7 @@ Ext.define('Comic.view.About', {
             xtype: 'textfield',
             name: 'version',
             label: 'Version',
-            value: '1.8',
+            value: '1.9',
             readOnly: true
           },
           {
