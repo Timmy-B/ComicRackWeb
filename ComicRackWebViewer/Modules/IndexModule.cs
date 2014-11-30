cd83d7e5a187d7e91ee3b11d1795619cd0929bf5
jeroenwalter <jeroen@enormkansloos.nl>
2014-11-30 19:53:17 +0100

    re-enabled redirect from / to /tablet/index.html

--- ComicRackWebViewer/Modules/IndexModule.cs
+++ ComicRackWebViewer/Modules/IndexModule.cs
@@ -1,14 +1,14 @@
-﻿//using Nancy;
-//using Nancy.Responses;
+﻿using Nancy;
+using Nancy.Responses;
 
-//namespace ComicRackWebViewer
-//{
-//    public class IndexModule : NancyModule
-//    {
-//        public IndexModule()
-//            : base("/")
-//        {
-//            Get["/"] = x => { return Response.AsRedirect("/tablet/index.html", RedirectResponse.RedirectType.Permanent); };
-//        }
-//    }
-//}
+namespace ComicRackWebViewer
+{
+  public class IndexModule : NancyModule
+  {
+    public IndexModule()
+      : base("/")
+    {
+      Get["/"] = x => { return Response.AsRedirect("/tablet/index.html", RedirectResponse.RedirectType.Permanent); };
+    }
+  }
+}
